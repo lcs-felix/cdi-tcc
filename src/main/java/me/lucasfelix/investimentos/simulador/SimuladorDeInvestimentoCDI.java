2f104ed7f415b855e2239e61ac7ce0693b3c9273
Lucas Felix <lfelixsampaio@gmail.com>
2016-10-11 19:53:01 -0300

    Logger no postcontructor dos simuladores

--- src/main/java/me/lucasfelix/investimentos/simulador/SimuladorDeInvestimentoCDI.java
+++ src/main/java/me/lucasfelix/investimentos/simulador/SimuladorDeInvestimentoCDI.java
@@ -1,11 +1,26 @@
 package me.lucasfelix.investimentos.simulador;
 
 import me.lucasfelix.investimentos.annotation.Simulador;
+import me.lucasfelix.investimentos.logger.Logger;
 import me.lucasfelix.investimentos.modelo.Investimento;
 import me.lucasfelix.investimentos.modelo.Titulo;
 
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
 @Simulador(investimento = Investimento.CDI)
 public class SimuladorDeInvestimentoCDI implements SimuladorDeInvestimento {
+
+    @Inject
+    private Logger logger;
+
+    public SimuladorDeInvestimentoCDI() {
+    }
+
+    @PostConstruct
+    public void inicio() {
+        logger.info("Iniciando o simulador CDI");
+    }
 
     @Override
     public Double retornoDoInvestimento(Titulo titulo) {
