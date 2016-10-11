8db45c067263ec2befa3622952bac213bb32e86d
Lucas Felix <lfelixsampaio@gmail.com>
2016-10-11 18:17:32 -0300

    Logger - erro pq nao tem producer

--- src/main/java/me/lucasfelix/investimentos/servlet/CompraTituloServlet.java
+++ src/main/java/me/lucasfelix/investimentos/servlet/CompraTituloServlet.java
@@ -2,6 +2,7 @@ package me.lucasfelix.investimentos.servlet;
 
 import me.lucasfelix.investimentos.logger.Logger;
 
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,9 +13,11 @@ import java.io.IOException;
 @WebServlet("/compra")
 public class CompraTituloServlet extends HttpServlet {
 
+    @Inject
+    private Logger logger;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Logger logger = new Logger(CompraTituloServlet.class);
         logger.info("Iniciando a compra de título");
     }
 }
